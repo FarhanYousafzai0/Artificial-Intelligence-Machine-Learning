78d132af329f614b57d512fa5ae814c973d12f78
Farhan Yousafzai <coderwithferry@gmail.com>
2025-12-02 18:17:29 +0500

    Object oriented programmming important pillar,Encapsulation,inheritence,Abstraction,polymorphism

--- class.py
+++ class.py
@@ -83,7 +83,20 @@ So there are three types of methods in python :
 * So class artributes only acces only class artribures get_laptop_storage(cls)
 
 * So static methods cant access class artributes neither instance artributes.So static methods are stand alone functions in the class.
+
+
+
+
+Notes :
+Almost everything in Python is an object, with its properties and methods.
+
+
 """
+
+
+
+
+
 
 
 # Question NO 1:
@@ -130,3 +143,188 @@ So there are three types of methods in python :
 
 
 # Object Oriented Programming Language Core Pillars => Important Topic :
+
+
+
+
+
+# class object:
+#     x = 5
+
+
+
+# p1 = object()
+
+
+
+# print(p1.x);
+
+
+# del p1
+
+# Note: The __init__() method is called automatically every time the class is being used to create a new object.
+
+
+# class Person:
+#     def __init__(self,name,age):
+#         self.name = name
+#         self.age = age
+
+
+
+# p1 = Person("Farhan",20);
+# p2 = Person("Ali",26);
+
+
+# print(f"So my name is {p1.name} and i am {p1.age} old.")
+# print(f"So my name is {p2.name} and i am {p2.age} old.")
+
+        
+
+
+
+
+
+# Create a class without iniit methot:Without the __init__() method, you would need to set properties manually for each object:
+
+# The pass Statement :class definitions cannot be empty, but if you for some reason have a class definition with no content, put in the pass statement to avoid getting an error.
+
+# class Student:
+#     pass
+
+
+
+
+# p1 = Student();
+# p1.name= "Farhan"
+# p1.age = 5
+
+
+# print(f"My name is {p1.name}")
+
+
+
+""" 
+Self parameter : 
+
+The self parameter is a reference to the current instance of the class.
+
+It is used to access properties and methods that belong to the class.
+
+
+"""
+
+
+
+
+# class Self:
+#     def __init__(self,name,age,school):
+#         self.name = name
+#         self.age = age
+#         self.school = school
+
+
+#         # great funciton:
+#     def greet(self):
+#         print(f"Helo my name is {self.name} and i am studing at {self.school} and my age is {self.age}");
+
+
+
+
+# s1 = Self("Farhan",20,"Fg Public High School Nowshera cantt.");
+
+# s1.greet();
+        
+
+
+# class Car:
+#     def __init__(self,brand,model,year):
+#         self.brand = brand
+#         self.model = model
+#         self.year = year
+
+#     def display_infor(self):
+#         print(f"The brand is |{self.brand}| ,the model is |{self.model}| and the year is |{self.year}|");
+   
+
+#     def let_buy(self):
+#         message = self.display_infor()
+#         print(f"So how this model did you like it or not.")
+
+# c1 = Car("BMW","M5 Compitition",2025);
+
+# c1.let_buy()
+                
+
+
+
+"""
+Encapsulations And Data hiding ,
+
+
+
+
+
+"""
+
+# Questiono:
+
+
+# class Bank:
+#     def __init__(self,name,balance):
+        
+#         self.name = name
+        # self.__balance = balance
+
+
+      # Getter Functions so get the values of private attributes:
+      # 
+      # 
+#     def get_info(self):
+
+#         return self.__balance
+
+
+#     def set_info(self,newbalance):
+#         self.__balance = newbalance 
+
+
+# b1 = Bank("Farhan",400000)
+
+
+# b1.set_info(200000);
+
+# print(b1.name,b1.get_info());
+
+
+# So there fist we get the value of private artributes and then set the values of privates by using the setter and getter functions.
+
+
+
+# Interitence:
+
+
+class Employe:
+    startTime = "10PM"
+    endTime = "6PM"
+
+    def change(self, new_time):
+        self.endTime = new_time
+
+
+class Teacher(Employe):
+    def __init__(self, subject):
+        self.subject = subject
+
+
+# Creating object
+t1 = Teacher("Mathematics")
+
+# Changing end time using inherited method
+t1.change("8PM")
+
+# Printing details
+print(f"The subject is {t1.subject}, the start time is {t1.startTime}, and the end time is {t1.endTime}")
+
+        
+        
